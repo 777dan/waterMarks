1884b92d85dee0766fbbc82ae87a0c0c7bf8024f
777dan <danya.nikitin7@gmail.com>
2022-12-06 17:27:55 +0200

    v 2.0

--- index.php
+++ index.php
@@ -3,22 +3,23 @@
 $string = "Nature";
 // Загрузка рисунка фона
 // $im = imageCreateFromJpeg("images/img2.jpg");
-$dirLength = count(scandir("images"))-2;
-for ($i=1; $i <= $dirLength; $i++) { 
-    $im = imageCreateFromJpeg("images/img$i.jpg");
+$dirLength = count(scandir("images")) - 2;
+if (!is_dir("newImages")){
+    mkdir("newImages");
 }
-// Создание в палитре нового цвета — черного.
-$color = imageColorAllocate($im, 255, 255, 0);
-// Размер шрифта
-$size = 5;
-// Сдвиг надписи
-$offset = strlen($string) / 2 * $size;
-// Вычисление x для расположения текста по ширине
-$x = (imageSX($im) - strlen($string)) / 2 - $offset;
-$y = 50;
-// Вывод строки поверх загруженного изображения.
-imageString($im, $size, $x, $y, $string, $color);
-// Вывод картинки в стандартный выходной поток - в браузер
-header("Content-type: image/png");
-imageJpeg($im); // освобождение памяти, занятой картинкой 
-imageDestroy($im);
+for ($i = 1; $i <= $dirLength; $i++) {
+    $im = imageCreateFromJpeg("images/img$i.jpg");
+    // Создание в палитре нового цвета — черного.
+    $color = imageColorAllocate($im, 255, 255, 0);
+    // Размер шрифта
+    $size = 10;
+    // Сдвиг надписи
+    $offset = strlen($string) / 2 * $size;
+    // Вычисление x для расположения текста по ширине
+    $x = (imageSX($im) - strlen($string)) / 2 - $offset;
+    $y = 50;
+    imageString($im, $size, $x, $y, $string, $color);
+    header("Content-type: image/png");
+    imageJpeg($im, "newImages/newImg$i.jpg");
+    imageDestroy($im);
+}
